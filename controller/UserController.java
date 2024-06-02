cb15a8891ebdf76d59602db5903353343728f00b
singhritik08 <115515962+singhritik08@users.noreply.github.com>
2024-06-02 13:41:39 +0530

    Add files via upload

--- controller/UserController.java
+++ controller/UserController.java
@@ -1,5 +1,7 @@
 package Feedback_System.controller;
 
+import Feedback_System.model.Batch;
+import Feedback_System.model.FeedbackQuestion;
 import Feedback_System.model.User;
 import Feedback_System.service.impl.BatchServiceImpl;
 import Feedback_System.service.impl.FeedbackServiceImpl;
@@ -9,59 +11,102 @@ import Feedback_System.service.impl.UserServiceImpl;
 import java.util.List;
 
 public class UserController {
-    UserServiceImpl studentService = new UserServiceImpl();
+    UserServiceImpl userService = new UserServiceImpl();
     BatchServiceImpl batchService = new BatchServiceImpl();
     QuestionServiceImpl questionService = new QuestionServiceImpl();
     FeedbackServiceImpl feedbackService = new FeedbackServiceImpl();
 
 
-    public String signUp(String username, String password, String phone, String role) {
-        return studentService.signUp(username, password, phone, role);
+    public boolean signUp(String username, String password, String phone, String role) {
+        return userService.signUp(username, password, phone, role);
     }
 
-    public String signIn(String username, String pass) {
-        return studentService.getUser(username, pass);
+    public boolean signIn(String username, String pass) {
+        return userService.getUser(username, pass);
     }
+
 
     public List<User> userList() {
-        return studentService.userList();
+        return userService.userList();
     }
-    public String assignBatch(String batchName, String studentPhone, String role, String adminPhone){
-        return batchService.assignBatch(batchName, studentPhone,role,adminPhone,studentService);
+    public String assignBatch(String batchName, String studentPhone, String adminPhone){
+        return batchService.assignBatch(batchName, studentPhone,adminPhone, userService);
     }
     public String createQuestion(String batchName,  String adminPhone) {
-        return questionService.createQuestion(batchName, adminPhone, studentService, batchService);
+        return questionService.createQuestion(batchName, adminPhone, userService, batchService);
     }
-    public void questionList(){
-        questionService.list();
+    public List<String> listOfQuestion(){
+        return questionService.questionList();
+    }
+    public void listOfQuestionByBatch(String batch) {
+        questionService.listOfQuestionByBatch(batch,batchService);
     }
 
     public String deleteQuestion(String qId,String batch, String adminPhone) {
-        return questionService.deleteQuestion(qId,batch, adminPhone, studentService, batchService, feedbackService);
+        return questionService.deleteQuestion(qId,batch, adminPhone, userService, batchService, feedbackService);
     }
     public String updateQuestion(String qId,String batch, String adminPhone) {
-        return questionService.updateQuestion(qId,batch, adminPhone, studentService, batchService);
+        return questionService.updateQuestion(qId,batch, adminPhone, userService, batchService);
     }
     public String submitFeedback(String studentPhone, String batchName) {
-        return feedbackService.submitFeedback(studentPhone, batchName, studentService,batchService,questionService);
+        return feedbackService.submitFeedback(studentPhone, batchName,batchService,questionService);
     }
-    public void feedbackList(){
-         feedbackService.feedbackList();
-    }
-    public String feedbackByBatch(String batch){
-        return feedbackService.listOfFeedbackByBatch(batch,batchService);
-    }
-    public String feedbackByBatchToAdmin(String adminPhone, String batch){
-        return feedbackService.feedbackByBatchToAdmin(adminPhone,batch,batchService,studentService);
+//    public void feedbackList(){
+//        feedbackService.feedbackList(questionService);
+//    }
+    public String feedbackByBatch(String adminPhone, String batch){
+        return feedbackService.feedbackByBatch(adminPhone,batch,batchService, userService);
     }
     public String individualFeedback(String adminPhone, String studentPhone){
-        return feedbackService.individualFeedback(adminPhone,studentPhone,studentService);
+        return feedbackService.individualFeedback(adminPhone,studentPhone, userService);
     }
     public void listWithQid(){
         questionService.listWithQid();
     }
     public String createBatch(String adminPhone, String batchName) {
-        return batchService.createBatch(adminPhone,batchName,studentService);
+        return batchService.createBatch(adminPhone,batchName, userService);
     }
 
+    public List<Batch> listOfBatches() {
+        return batchService.getListOfBatch();
+    }
+
+    public String getPhoneByPassword(String password) {
+        return userService.findPhoneByPassword(password);
+    }
+
+    public String findRoleByPhoneNumber(String phoneNumber) {
+        return userService.findRoleByPhoneNumber(phoneNumber);
+    }
+
+    public String getYourBatch(String username, String password) {
+        return batchService.findBatchByUsernameAndPassword(username,password);
+    }
+
+    public List myBatchMates(String username, String password) {
+        return batchService.showBatchMates(username,password);
+    }
+
+    public boolean madeBatch(String batchName, String username) {
+        return batchService.madeBatch(batchName,username);
+    }
+    public boolean existsBatch(String batchName){
+        return batchService.existsBatch(batchName);
+    }
+
+    public void listOfStudentByBatch(String batchName) {
+        batchService.listOfStudentByBatchName(batchName);
+    }
+    public String deleteBatch(String adminPhone, String batchName) {
+        return batchService.deleteBatch(adminPhone, batchName,userService,batchService);
+    }
+
+    public boolean isBatchExists() {
+        return batchService.isBatchExists();
+    }
+
+
+    public int isPhoneExist(String phoneNumber, String username) {
+        return userService.isPhoneExist(phoneNumber , username);
+    }
 }
